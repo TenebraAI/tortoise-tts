b8adda1230e06f26baa08d93315ffb5a47b9ebaf
Barak Avrahami <baraka@coregen.io>
2023-12-23 00:52:46 +0200

    Fixed tokenizer path

--- tortoise/utils/tokenizer.py
+++ tortoise/utils/tokenizer.py
@@ -10,6 +10,8 @@ from tokenizers import Tokenizer
 # Regular expression matching whitespace:
 from unidecode import unidecode
 
+import tortoise
+
 _whitespace_re = re.compile(r'\s+')
 
 
@@ -167,7 +169,7 @@ def lev_distance(s1, s2):
   return distances[-1]
 
 
-DEFAULT_VOCAB_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../data/tokenizer.json')
+DEFAULT_VOCAB_FILE = os.path.join(os.path.dirname(os.path.realpath(tortoise.__file__)), 'data/tokenizer.json')
 
 
 class VoiceBpeTokenizer:
